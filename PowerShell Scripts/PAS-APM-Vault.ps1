fb0674cc6133d65f164ddf0e1c6b8ebc705e407e
jcreameriii <43750749+jcreameriii@users.noreply.github.com>
2019-11-18 17:56:41 -0500

    Update PAS-APM-Vault.ps1

--- PowerShell Scripts/PAS-APM-Vault.ps1
+++ PowerShell Scripts/PAS-APM-Vault.ps1
@@ -2,13 +2,14 @@
 $HostName = "VAULT"
 $PORT = 51444
 $SYSLOGSERVER="10.0.0.2"
+$Version = "1.0.0000"
+
 #PrivateArk Server Service Check
+$MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "PrivateArk Server" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "PrivateArk Server" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$Date = Get-Date
-$DateTime = $DATE.ToString("yyyy-MM-ddTHH:mm:ssZ")
-$syslogoutput = "$DateTime, $HostName, $ServiceName, $ServiceStatus, $ServiceStatusNumeric"
+$syslogoutput = "CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
@@ -20,12 +21,11 @@ $ByteSyslogMessage = $Encoding.GetBytes(''+$syslogoutputclean+'')
 $UDPCLient.Send($ByteSyslogMessage, $ByteSyslogMessage.Length)
 
 #PrivateArk Database Service Check
+$MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "PrivateArk Database" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "PrivateArk Database" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$Date = Get-Date
-$DateTime = $DATE.ToString("yyyy-MM-ddTHH:mm:ssZ")
-$syslogoutput = "$DateTime, $HostName, $ServiceName, $ServiceStatus, $ServiceStatusNumeric"
+$syslogoutput = "CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
@@ -37,12 +37,11 @@ $ByteSyslogMessage = $Encoding.GetBytes(''+$syslogoutputclean+'')
 $UDPCLient.Send($ByteSyslogMessage, $ByteSyslogMessage.Length)
 
 #CyberArk Logic Container Service Check
+$MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "CyberArk Logic Container" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "CyberArk Logic Container" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$Date = Get-Date
-$DateTime = $DATE.ToString("yyyy-MM-ddTHH:mm:ssZ")
-$syslogoutput = "$DateTime, $HostName, $ServiceName, $ServiceStatus, $ServiceStatusNumeric"
+$syslogoutput = "CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
@@ -54,12 +53,11 @@ $ByteSyslogMessage = $Encoding.GetBytes(''+$syslogoutputclean+'')
 $UDPCLient.Send($ByteSyslogMessage, $ByteSyslogMessage.Length)
 
 #PrivateArk Remote Control Agent Service Check
+$MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "PrivateArk Remote Control Agent" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "PrivateArk Remote Control Agent" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$Date = Get-Date
-$DateTime = $DATE.ToString("yyyy-MM-ddTHH:mm:ssZ")
-$syslogoutput = "$DateTime, $HostName, $ServiceName, $ServiceStatus, $ServiceStatusNumeric"
+$syslogoutput = "CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
@@ -71,12 +69,11 @@ $ByteSyslogMessage = $Encoding.GetBytes(''+$syslogoutputclean+'')
 $UDPCLient.Send($ByteSyslogMessage, $ByteSyslogMessage.Length)
 
 #Cyber-Ark Event Notification Engine Service Check
+$MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "Cyber-Ark Event Notification Engine" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "Cyber-Ark Event Notification Engine" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$Date = Get-Date
-$DateTime = $DATE.ToString("yyyy-MM-ddTHH:mm:ssZ")
-$syslogoutput = "$DateTime, $HostName, $ServiceName, $ServiceStatus, $ServiceStatusNumeric"
+$syslogoutput = "CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
