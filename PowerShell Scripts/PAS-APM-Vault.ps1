e3af1786d7596854fb1b518373c02f66c7fb787b
jcreameriii <43750749+jcreameriii@users.noreply.github.com>
2019-11-27 14:04:03 -0500

    Update PAS-APM-Vault.ps1

--- PowerShell Scripts/PAS-APM-Vault.ps1
+++ PowerShell Scripts/PAS-APM-Vault.ps1
@@ -11,7 +11,9 @@ $MonitorType = "ApplicationMonitor"
 $ServiceName = Get-Service "PrivateArk Server" | Format-Table -HideTableHeaders Name | Out-String
 $ServiceStatus = Get-Service "PrivateArk Server" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
-$syslogoutput = "$DateTime CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
+$SoftwareName = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object DisplayName -like "*CyberArk Digital Vault*" | Select-Object DisplayName | Select -first 1 | Format-Table -HideTableHeaders | Out-String
+$SoftwareVersion = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object DisplayName -like "*CyberArk Digital Vault*" | Select-Object DisplayVersion | Select -first 1 | Format-Table -HideTableHeaders | Out-String
+$syslogoutput = "$DateTime CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|$SoftwareName|$SoftwareVersion"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
@@ -76,6 +78,23 @@ $ServiceName = Get-Service "Cyber-Ark Event Notification Engine" | Format-Table 
 $ServiceStatus = Get-Service "Cyber-Ark Event Notification Engine" | Format-Table -HideTableHeaders Status | Out-String
     If ($ServiceStatus -like "*Running*") { $ServiceStatusNumeric = 1 } else { $ServiceStatusNumeric = 0 }
 $syslogoutput = "$DateTime CEF:0|CyberArk|$MonitorType|$Version|$HostName|$ServiceName|$ServiceStatus|$ServiceStatusNumeric|"
+#cleanup command to remove new lines and carriage returns
+$syslogoutputclean = $syslogoutput -replace "`n|`r"
+$syslogoutputclean | ConvertTo-Json
+#send syslog to SIEM
+$UDPCLient = New-Object System.Net.Sockets.UdpClient
+$UDPCLient.Connect($SYSLOGSERVER, $PORT)
+$Encoding = [System.Text.Encoding]::ASCII
+$ByteSyslogMessage = $Encoding.GetBytes(''+$syslogoutputclean+'')
+$UDPCLient.Send($ByteSyslogMessage, $ByteSyslogMessage.Length)
+
+#OS System Information
+$MonitorType = "OSMonitor"
+$OSName = (Get-WmiObject Win32_OperatingSystem).Caption | Out-String
+$OSVersion = (Get-WmiObject Win32_OperatingSystem).Version | Out-String
+$OSServPack = (Get-WmiObject Win32_OperatingSystem).ServicePackMajorVersion | Out-String
+$OSArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture | Out-String
+$syslogoutput = "$DateTime CEF:0|CyberArk|$MonitorType|$Version|$HostName|$OSName|$OSVersion|$OSServPack|$OSArchitecture"
 #cleanup command to remove new lines and carriage returns
 $syslogoutputclean = $syslogoutput -replace "`n|`r"
 $syslogoutputclean | ConvertTo-Json
